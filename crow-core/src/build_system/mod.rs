3c1dd4371a47df1339679a873ed7e404c340c8cb
user-with-username <you@example.com>
2025-07-25 21:12:24 +0700

    Refactor git manager

--- crow-core/src/build_system/mod.rs
+++ crow-core/src/build_system/mod.rs
@@ -1,13 +1,13 @@
 mod builder;
 mod cache;
 mod dependency;
-mod git_manager;
+mod manager;
 mod toolchain;
 
 pub use builder::BuildSystem;
 pub use cache::BuildCache;
 pub use dependency::{DependencyBuildOutput, DependencyResolver};
-pub use git_manager::GitManager;
+pub use manager::GitManager;
 pub use toolchain::ToolchainExecutor;
 
 use crate::config::{BuildProfile, Config, PackageConfig, ToolchainConfig};
