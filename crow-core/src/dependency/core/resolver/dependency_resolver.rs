9cde51444f20591ef564b232546ae33e7eed44e0
user-with-username <anybodyy69@gmail.com>
2026-05-24 22:15:56 +0700

    chore: fix linking for header only projects

--- crow-core/src/dependency/core/resolver/dependency_resolver.rs
+++ crow-core/src/dependency/core/resolver/dependency_resolver.rs
@@ -285,13 +285,14 @@ impl DependencyResolver {
                 resolved.include_dirs.push(include_dir);
             }
 
-            if Self::is_linkable_type(&package.r#type) {
-                let lib_path = package.output_path_in(&target_dir.join(profile_name));
-                let link_item = lib_path.to_string_lossy().to_string();
-                if seen_libs.insert(link_item.clone()) {
-                    resolved.libs.push(link_item);
-                }
-            }
+           let lib_path = package.output_path_in(&target_dir.join(profile_name));
+if Self::is_linkable_type(&package.r#type) && lib_path.exists() {
+    let link_item = lib_path.to_string_lossy().to_string();
+    if seen_libs.insert(link_item.clone()) {
+        resolved.libs.push(link_item);
+    }
+}
+
         }
 
         resolved.max_standard = max_standard.map(|s| s.to_string());
